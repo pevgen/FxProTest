c13062bdead603cd81c5f8830f9390ba4abde9e3
pevgen <pevgen@gmail.com>
2021-08-05 13:24:17 +0300

    added local extremes calculation

--- src/main/java/fxpro/WaterCalculator.java
+++ src/main/java/fxpro/WaterCalculator.java
@@ -1,5 +1,8 @@
 package fxpro;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.util.Objects.isNull;
 
 public class WaterCalculator {
@@ -13,6 +16,35 @@ public class WaterCalculator {
     public long calculateWaterAmount(int[] landscape) {
         checkLandscapeSize(landscape);
         return 0;
+    }
+
+    //TODO
+    protected int[] calcMaxWaterLevels(int[] landscape, Map<Integer, Integer> localExtremes) {
+        return null;
+    }
+
+    protected Map<Integer, Integer> calcLocalExtremes(int[] landscape) {
+        Map<Integer, Integer> localExtremes = new HashMap<>();
+        // left border
+        if (landscape[0] > landscape[1]) {
+            localExtremes.put(0, landscape[0]);
+        }
+        // in the middle
+        int currentMaxIndex = 0;
+        for (int i = 0; i < landscape.length - 1; i++) {
+            if (landscape[i] < landscape[i + 1]) {
+                currentMaxIndex = i + 1;
+            }
+            if (landscape[i] > landscape[i + 1]) {
+                localExtremes.put(currentMaxIndex, landscape[currentMaxIndex]);
+            }
+        }
+        // right border
+        if (landscape[landscape.length - 1] > landscape[landscape.length - 2]) {
+            localExtremes.put(landscape.length - 1, landscape[landscape.length - 1]);
+        }
+
+        return localExtremes;
     }
 
     private void checkLandscapeSize(int[] landscape) throws IllegalArgumentException, IndexOutOfBoundsException {
--- src/test/java/fxpro/WaterCalculatorTest.java
+++ src/test/java/fxpro/WaterCalculatorTest.java
@@ -3,34 +3,95 @@ package fxpro;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class WaterCalculatorTest {
 
     private WaterCalculatorConfig waterCalculatorConfig;
+    private WaterCalculator waterCalculator;
 
     @Before
     public void before() {
         waterCalculatorConfig = new WaterCalculatorConfig();
+        waterCalculator = new WaterCalculator(waterCalculatorConfig);
+    }
+
+
+    @Test
+    public void testCalcMaxWaterLevels(){
+        int[] result = waterCalculator.calcMaxWaterLevels(null, null);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testLocalExtremaFewHillsWithPlateaus() {
+            int[] landscape = new int[]{5, 1, 1, 5, 5, 4, 0, 0, 0};
+            Map<Integer, Integer> result = waterCalculator.calcLocalExtremes(landscape);
+            assertEquals(2, result.size());
+            assertEquals(Integer.valueOf(5), result.get(0));
+            assertEquals(Integer.valueOf(5), result.get(3));
+    }
+    
+    
+    @Test
+    public void testLocalExtremaFewHills() {
+        int[] landscape = new int[]{5, 1, 2, 4, 5, 4, 0, 3, 1};
+        Map<Integer, Integer> result = waterCalculator.calcLocalExtremes(landscape);
+        assertEquals(3, result.size());
+        assertEquals(Integer.valueOf(5), result.get(0));
+        assertEquals(Integer.valueOf(5), result.get(4));
+        assertEquals(Integer.valueOf(3), result.get(7));
+    }
+
+    @Test
+    public void testLocalExtremaOnePit() {
+        int[] landscape = new int[]{2, 1, 3};
+        Map<Integer, Integer> result = waterCalculator.calcLocalExtremes(landscape);
+        assertEquals(2, result.size());
+        assertEquals(Integer.valueOf(2), result.get(0));
+        assertEquals(Integer.valueOf(3), result.get(2));
+    }
+
+    @Test
+    public void testLocalExtremaOneHill() {
+        int[] landscape = new int[]{1, 3, 1};
+        Map<Integer, Integer> result = waterCalculator.calcLocalExtremes(landscape);
+        assertEquals(1, result.size());
+        assertEquals(Integer.valueOf(3), result.get(1));
+    }
+
+    @Test
+    public void testLocalExtremaOnlyDecreasing() {
+        int[] landscape = new int[]{3, 2, 1};
+        Map<Integer, Integer> result = waterCalculator.calcLocalExtremes(landscape);
+        assertEquals(1, result.size());
+        assertEquals(Integer.valueOf(3), result.get(0));
+    }
+
+    @Test
+    public void testLocalExtremaOnlyIncreasing() {
+        int[] landscape = new int[]{1, 2, 3};
+        Map<Integer, Integer> result = waterCalculator.calcLocalExtremes(landscape);
+        assertEquals(1, result.size());
+        assertEquals(Integer.valueOf(3), result.get(2));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testExecuteWithNullLandscape() {
-        WaterCalculator waterCalculator = new WaterCalculator(waterCalculatorConfig);
         waterCalculator.calculateWaterAmount(null);
         fail("Should throw IllegalArgumentException if landscape is null");
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testExecuteWithEmptyLandscape() {
-        WaterCalculator waterCalculator = new WaterCalculator(waterCalculatorConfig);
         waterCalculator.calculateWaterAmount(new int[]{1, 2});
         fail("Should throw IllegalArgumentException if landscape is too small");
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testExecuteWithOversizedLandscape() {
-        WaterCalculator waterCalculator = new WaterCalculator(waterCalculatorConfig);
         waterCalculator.calculateWaterAmount(new int[waterCalculatorConfig.getMaxLandscapePosition() + 1]);
         fail("Should throw IllegalArgumentException if landscape is too big");
     }
